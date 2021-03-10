7eb5ede0b805efa59120f1713067127c763b0e0f
Naman <namanarora311@gmail.com>
2021-03-10 23:48:21 +0530

    opened crick buzz matches

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 require('chromedriver');
 let wd = require('selenium-webdriver');
 let browser = new wd.Builder().forBrowser('chrome').build();
-
+let matchId=35193;
+browser.get(`https://www.cricbuzz.com/live-cricket-scores/${matchId}`);
