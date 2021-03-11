4b422a82ce88228e04e0efab6fa2e37e2e1542ea
Naman <namanarora311@gmail.com>
2021-03-11 22:03:39 +0530

    second button clicked

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ require('chromedriver');
 let wd = require('selenium-webdriver');
 let browser = new wd.Builder().forBrowser('chrome').build();
 let matchId=35193;
-browser.get(`https://www.cricbuzz.com/live-cricket-scores/${matchId}`);
+
+async function main(){
+await browser.get(`https://www.cricbuzz.com/live-cricket-scores/${matchId}`);
+await browser.wait(wd.until.elementLocated(wd.By.css(".cb-nav-bar a")));
+let buttons = await (await browser).findElements(wd.By.css(".cb-nav-bar a"));
+await buttons[1].click();
+
+}
+main();
+
