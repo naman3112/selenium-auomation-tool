f305e7ff8888d6902b388e693c8492c89899e334
Naman <namanarora311@gmail.com>
2021-03-11 23:21:03 +0530

    selenium for tags for automation

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ await browser.get(`https://www.cricbuzz.com/live-cricket-scores/${matchId}`);
 await browser.wait(wd.until.elementLocated(wd.By.css(".cb-nav-bar a")));
 let buttons = await (await browser).findElements(wd.By.css(".cb-nav-bar a"));
 await buttons[1].click();
-
+await browser.wait(wd.until.elementLocated(wd.By.css(".cb-col.cb-col-100.cb-ltst-wgt-hdr")));
+let tables =  await browser.findElements(wd.By.css(".cb-col.cb-col-100.cb-ltst-wgt-hdr"));
+console.log( tables.length +"in the ");
 }
 main();
 
